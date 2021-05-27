4fddad7a7b6d9879cb119bdb1dd7443c6f661d98
akudaibergen <you@example.com>
2021-05-27 14:48:03 +0600

    5-6

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,3 +1,4 @@
+
 object Hello extends App {
   //Valid Anagram
   def isAnagram(s: String, t: String): Boolean = {
@@ -93,6 +94,94 @@ object Hello extends App {
   }
 //  var t = intersection(Array(1,2,2,3), Array(1,2,2,4,6,63))
 
+  //Maximum Units on a Truck
+  def maximumUnits(boxTypes: Array[Array[Int]], truckSize: Int): Int = {
+    var output = 0
+    var count = 0
+    var isOk = true
+    //sorting to add the big boxes
+    var sortedBox = boxTypes.sortBy(_(1))
+    var i = sortedBox.length-1
+
+    //adding while is ok
+    while (isOk && i>=0){
+      count = count + sortedBox(i)(0)
+      if(count<truckSize){
+        var temp = sortedBox(i)(0)*sortedBox(i)(1)
+        output = output + temp
+        i = i - 1
+      }
+      else{
+        var dif = count - truckSize
+        var temp = (sortedBox(i)(0)-dif) * sortedBox(i)(1)
+        output = output + temp
+        i = i - 1
+        isOk = false
+      }
+    }
+    output
+  }
+//  var t = maximumUnits(Array(Array(2,5),Array(5,10),Array(4,7),Array(3,9)),10)
 
 
+  //increasing Decreasing String
+  def sortString(s: String): String = {
+    var sortedString = s.sorted
+    val builder = List.newBuilder[Int]
+    var string = s.sorted.distinct
+    //counting each element
+    for(i <- string) {
+      var count = 0
+      for(t<- sortedString) {
+        if(i == t) {
+          count = count + 1
+        }
+      }
+      builder += count
+    }
+//    println(string)
+    var counts = builder.result().toArray
+
+    var output = ""
+    var isOk = true
+    var reverse = false
+    //adding until it has equal length with s
+    while (output.length  < s.length) {
+      //add in abs
+      if(!reverse) {
+        //adding each element by count if he has count more than 0
+        for( i <- 0 to string.length-1) {
+          if(counts(i) > 0) {
+            output += string(i)
+            counts.update(i,(counts(i)-1))
+            println(string(i) + " " + counts(i))
+          }
+        }
+        println("reverseIsnot" + output)
+        reverse = true
+      }
+      //add in desc
+      else {
+        //adding each element by count if he has count more than 0
+        for( i <- string.length-1 to 0 by -1) {
+          if(counts(i) > 0) {
+            output += string(i)
+            counts.update(i,(counts(i)-1))
+          }
+        }
+        println("reverse" + output)
+        reverse = false
+      }
+    }
+
+    output
+  }
+
+  var s = sortString("leetcode")
+  println(s)
+
+//  leetcode
+//  cdelottoled
+//  leetcode
+//  cdelotee
 }
